d22560670267460c666bb7a15ad71aec1cbebb10
Yiwei <mithril0216@gmail.com>
2019-04-26 07:03:41 +0800

    增加一些 shopItem 的 action

--- src/components/shopItem/action/index.js
+++ src/components/shopItem/action/index.js
@@ -1,8 +1,14 @@
 import { createActions } from 'redux-actions';
-import { ADD_TO_CHART } from './../constants/index';
-const { addToChart } = createActions({
-  ADD_TO_CHART: shopItem => ({ itemId: shopItem.itemId }),
-  REMOVE_FROM_CHART: shopItem => ({ itemId: shopItem.itemId })
-});
 
-export default { addToChart };
+const { fetchProductById, addToChart, removeFromChart, chartItemIncrease, chartItemDerease, fetchProducts } = createActions(
+  {
+    FETCH_PRODUCT_BY_ID: itemId => itemId,
+    ADD_TO_CHART: shopItem => ({ itemId: shopItem.itemId }),
+    CHART_ITEM_REMOVE: shopItemId => shopItemId,
+    CHART_ITEM_COUNT_INCREASE: shopItemId => shopItemId,
+    CHART_ITEM_COUNT_DECREASE: shopItemId => shopItemId
+  },
+  'FETCH_PRODUCTS'
+);
+
+export default { fetchProductById, addToChart, removeFromChart, chartItemIncrease, chartItemDerease, fetchProducts };
