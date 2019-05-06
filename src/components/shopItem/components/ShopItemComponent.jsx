03fd41277dc764df7288a66283d8e6277c91ddf7
Yiwei <mithril0216@gmail.com>
2019-05-07 06:24:37 +0800

    修正shopItem 一些function的定義方式避免編譯時的警告,另外修正分頁的錯誤

--- src/components/shopItem/components/ShopItemComponent.jsx
+++ src/components/shopItem/components/ShopItemComponent.jsx
@@ -6,26 +6,29 @@ import { ShopContext } from '../context';
 import { setItems, setPageItems } from '../context/action';
 
 const ShopItemComponent = () => {
+  const pageSize = 5;
   const { state, dispatch } = useContext(ShopContext);
-  const [pageIndex, setPageIndex] = useState(0);
+  const [pageIndex, setPageIndex] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
 
-  const handlePaginationChange = (e, { activePage }) => {
+  function handlePaginationChange(e, { activePage }) {
+    console.info(`switching page to ${activePage}`);
     setPageIndex(activePage);
-  };
+  }
 
-  const loadPagedItems = async pPageIndex => {
+  async function loadPagedItems(pPageIndex) {
     console.info(`loading page ${pPageIndex}`);
-
-    const pagedItems = shopItems.slice(pageIndex * 10, 10);
+    const source = shopItems;
+    const startIndex = pPageIndex * pageSize;
+    const pagedItems = source.slice(startIndex, startIndex + pageSize);
     dispatch(setPageItems(pPageIndex, pagedItems));
-  };
+  }
 
   useEffect(() => {
     console.info('ShopItemComponent effect!');
 
     // 這裡加入根據頁數取得對應的資料,並塞進ShopContext裡
-    loadPagedItems(pageIndex);
+    loadPagedItems(pageIndex - 1);
     return () => {
       console.info('ShopItemComponent unmounted');
     };
@@ -43,14 +46,14 @@ const ShopItemComponent = () => {
           </Grid.Row>
         </Grid>
         <Pagination
-          activePage={pageIndex + 1}
+          activePage={pageIndex}
           ellipsisItem={{ content: <Icon name="ellipsis horizontal" />, icon: true }}
           firstItem={{ content: <Icon name="angle double left" />, icon: true }}
           lastItem={{ content: <Icon name="angle double right" />, icon: true }}
           prevItem={{ content: <Icon name="angle left" />, icon: true }}
           nextItem={{ content: <Icon name="angle right" />, icon: true }}
           onPageChange={handlePaginationChange}
-          totalPages={shopItems.length / 10}
+          totalPages={shopItems.length / pageSize}
         />
       </Container>
     );
